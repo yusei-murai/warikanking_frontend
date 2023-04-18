666810af2d25d9a756e6b90562d46149100ac69f
yusei <yuseitbn@gmail.com>
2023-04-18 23:05:14 +0900

    eventガワだけ

---
+++ lib/views/events/event_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class EventListPage extends StatelessWidget {
+  const EventListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<QuerySnapshot>(
+
+      ),
+    )
+  }
+}
